42110c5e52a928710347778ceb1283b33fe0509b
jasperjones <jasperjones01@protonmail.com>
2022-04-05 18:50:45 -0500

    completed model one
    completed model one just as it should work. looking to add winning functions.

--- js.js
+++ js.js
@@ -1,24 +1,62 @@
 //Model 2
 
+//Generate possible choices
+
+const choiceArray = ['rock', 'paper', 'scissors'];
+
+
 //Generate computers choice by random from array of [rock, paper, scissors] = pcChoice;
 
+const pcChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+
+
 //Request or prompt for choice and save in a variable = yourChoice;
+
+let yourChoice = prompt('Rock, Paper, Scissors. Whats your choice?').toLowerCase();
+
+
+while (yourChoice != 'rock' && yourChoice != 'paper' && yourChoice != 'scissors' ){
+    alert ('Please Input a correct choice');
+    yourChoice = prompt('rock, paper, scissors. Whats your choice?').toLowerCase();
+
+};
+
 
 //Function rockPaperScissors
 //bassed on checking choice, computer console logs verdict.
 
-//if your choice is rock
-   
+function rockPaperScissors(rock, paper, scissors){
 
-//if your choice is paper
- 
- 
-//if your choice is scissors, possible answers
-   
+    if (yourChoice === pcChoice){
+        console.log('it\'s a tie');
+    }
 
-//if your choice is a tie then console log it's a tie
-   
+    //if your choice is wins
 
+    else if (yourChoice === 'rock' && pcChoice === 'scissors' 
+            || yourChoice === 'paper' && pcChoice === 'rock' 
+            || yourChoice === 'scissors' && pcChoice === 'paper')
+            {
+                console.log(`You Won! ${yourChoice} beats ${pcChoice}`);
+            }
+
+    ////if your choice is loses
+
+    else if (yourChoice === 'rock' && pcChoice === 'paper' 
+                || yourChoice === 'paper' && pcChoice === 'scissors' 
+                || yourChoice === 'scissors' && pcChoice === 'rock')
+                {
+                    console.log(`You Lose! ${pcChoice} beats ${yourChoice}`); 
+                } 
+
+    else {
+        console.log('Input a choice');
+    }   
+
+    }
 
 //call Function rockPaperScissors();
 
+rockPaperScissors();
+
+// add function to add scores
