cd1410cd113b42c78ffa8080b7f9d8506bb43c1d
jasperjones <jasperjones01@protonmail.com>
2022-04-05 12:11:23 -0500

    finished with psuedocode

---
+++ js.js
@@ -0,0 +1,24 @@
+//Model 2
+
+//Generate computers choice by random from array of [rock, paper, scissors] = pcChoice;
+
+//Request or prompt for choice and save in a variable = yourChoice;
+
+//Function rockPaperScissors
+//bassed on checking choice, computer console logs verdict.
+
+//if your choice is rock
+   
+
+//if your choice is paper
+ 
+ 
+//if your choice is scissors, possible answers
+   
+
+//if your choice is a tie then console log it's a tie
+   
+
+
+//call Function rockPaperScissors();
+
