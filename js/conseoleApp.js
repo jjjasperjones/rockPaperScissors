18a9cadfd6ff8afe6a1652ff9ac345089cb2e129
jasperjones <jasperjones01@protonmail.com>
2022-05-24 16:43:45 -0500

    added cosoleApp.js

--- js/conseoleApp.js
+++ js/conseoleApp.js
@@ -1,121 +1,97 @@
-//Assisgning icons to id to add event listners
-const choiceRock = document.querySelector('#rock');
-const choicePaper = document.querySelector('#paper');
-const choiceScissors = document.querySelector('#scissors');
+//Model 2
 
-//Delcaring other buttons
-const restart = document.querySelector('.restart-game');
-const scoreBoardMsg = document.querySelector('#score-board-msg');
-const myPoint = document.querySelector('#my-point');
-const pcPoint = document.querySelector('#pc-point');
+//Generate possible choices
+
+const choiceArray = ['rock', 'paper', 'scissors'];
 
 // Declare scores
-let yourScore = 0; 
+let yourScore = 0;
 let pcScore = 0;
 
-//Generate computers choice by random from array of [rock, paper, scissors] = pcChoice;
-function pcChoices(){
-    const choiceArray = ['rock', 'paper', 'scissors'];
+//Function rockPaperScissors
+//bassed on checking choice, computer console logs verdict.
 
-    pcChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+function rockPaperScissors(){
+    
+    //Generate computers choice by random from array of [rock, paper, scissors] = pcChoice;
 
-    return pcChoice;
-    };
+    let pcChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+    
+    //Request or prompt for choice and save in a variable = yourChoice;
 
-//End game function
-function endGame(){
-        if (yourScore === 5 || pcScore === 5) {
-            if (yourScore > pcScore) {
-            alert("You Win! Let's Party! :)");
-            } else if (pcScore > yourScore) {
-            alert("You Lost! Party Is Over! :(");
-            } else if (pcScore === yourScore) {
-            alert("Game Was A Tie. Try Again");
-            }
-            
-            restartGame();
-            return;
-        }
-    };
+    let yourChoice = prompt('Rock, Paper, Scissors. Whats your choice?').toLowerCase();
 
-//Restart game functioon
-function restartGame(){
-    window.location.reload(true);
-}
+    //Loop untill input is valid
 
-//Update scores function
-function updateScore(){
-    myPoint.innerHTML = yourScore;
-    pcPoint.innerHTML = pcScore;
-}
+    while (yourChoice != 'rock' 
+        && yourChoice != 'paper' 
+        && yourChoice != 'scissors' ){
+            alert ('Please Input a correct choice');
+            yourChoice = prompt('rock, paper, scissors. Whats your choice?').toLowerCase();
 
-function scoreMessages(scoreMessage){
-
-    scoreBoardMsg.textContent = scoreMessage;    
-}
-
-//Play rock paper scissors game
-function rockPaperScissors(yourChoice, pcChoice){
-    yourChoice = this.dataset.id;
-    pcChoice = pcChoices();
-
-    console.log(yourChoice);
-    console.log(pcChoice);
-
-//    console.log(yourScore);
-//    console.log(pcScore);
+};
 
 
-    if (yourScore < 5 && pcScore < 5 ) {
     if (yourChoice === pcChoice){
         yourScore += 0;
-        scoreMessage = 'It\'s a tie';
-        scoreMessages(scoreMessage);       
-        //console.log(scoreMessage);
-        //let itsDraw = 'it\'s a tie';
-        //console.log(itsDraw);
-        //let scoreboard = `Your Score = ${yourScore} : Pc Score = ${pcScore}`;
-        //console.log(scoreboard);
-        updateScore();    
-        endGame();      
+        let itsDraw = 'it\'s a tie';
+        console.log(itsDraw);
+        let scoreboard = `Your Score = ${yourScore} : Pc Score = ${pcScore}` 
+            return scoreboard;
     }
     
 
-    //if your choice wins
+    //if your choice is wins
+
     else if (yourChoice === 'rock' && pcChoice === 'scissors' ||
-             yourChoice === 'paper' && pcChoice === 'rock' ||
-             yourChoice === 'scissors' && pcChoice === 'paper') {   
-                pcScore.textContent = `${yourScore} += 0`;
+            yourChoice === 'paper' && pcChoice === 'rock' ||
+            yourChoice === 'scissors' && pcChoice === 'paper')
+            {   
                 yourScore++;
-                scoreMessage = `You Won! ${yourChoice} beats ${pcChoice}`;
-                scoreMessages(scoreMessage); 
-                //console.log(youWin);
-                //let scoreboard = `Your Score = ${yourScore} : Pc Score = ${pcScore}`;
-                //console.log(scoreboard);
-                updateScore();
-                endGame();
+                let youWin = `You Won! ${yourChoice} beats ${pcChoice}`;
+                console.log(youWin);
+                let scoreboard = `Your Score = ${yourScore} : Pc Score = ${pcScore}` 
+                    return scoreboard;
             }
 
-    ////if your choice loses
+    ////if your choice is loses
+
     else if (yourChoice === 'rock' && pcChoice === 'paper' ||
-             yourChoice === 'paper' && pcChoice === 'scissors' ||
-             yourChoice === 'scissors' && pcChoice === 'rock') {
+                yourChoice === 'paper' && pcChoice === 'scissors' ||
+                yourChoice === 'scissors' && pcChoice === 'rock'){
+
                     pcScore++;
-                    scoreMessage = `You Lose! ${pcChoice} beats ${yourChoice}`;
-                    scoreMessages(scoreMessage); 
-                    //youLose = `You Lose! ${pcChoice} beats ${yourChoice}`;
-                    //console.log(youLose); 
-                    //let scoreboard = `Your Score = ${yourScore} : Pc Score = ${pcScore}`; 
-                    //console.log(scoreboard);
-                    updateScore();
-                    endGame();
+                    youLose = `You Lose! ${pcChoice} beats ${yourChoice}`;
+                    console.log(youLose); 
+                    let scoreboard = `Your Score = ${yourScore} : Pc Score = ${pcScore}` 
+                        return scoreboard;
                 } 
+            
+ 
+
     }
 
-}
+    function gameOver() {
+        if (yourScore > pcScore) {
+            console.log("Game Over! You Win! :)");
+        } else if (pcScore > yourScore) {
+            console.log("Game Over! You Lost! :(");
+        }
+    }
+    
+    function startRockPaperScissors() {
+        console.log(rockPaperScissors());
+        if(yourScore < 5 && pcScore < 5){
+            startRockPaperScissors();
+        }
+        else{
+            gameOver();
+        }
+    }
+    
+    
+    startRockPaperScissors();
 
+//call Function rockPaperScissors();
 
-choiceRock.addEventListener('click', rockPaperScissors);
-choicePaper.addEventListener('click', rockPaperScissors);
-choiceScissors.addEventListener('click', rockPaperScissors);
-restart.addEventListener('click', restartGame);
+// add function to add scores
